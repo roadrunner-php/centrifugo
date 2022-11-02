9c353e32b0ec075a587540aeb21328bc4773a1ba
butschster <butschster@gmail.com>
2022-11-02 19:21:03 +0400

    Adds the ability to get RequestType from request object

--- src/RequestType.php
+++ src/RequestType.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace RoadRunner\Centrifugo;
 
+use RoadRunner\Centrifugo\Exception\InvalidRequestTypeException;
+
 enum RequestType: string
 {
     case Connect = 'connect';
@@ -11,4 +13,18 @@ enum RequestType: string
     case Publish = 'publish';
     case Subscribe = 'subscribe';
     case RPC = 'rpc';
+
+    public static function createFrom(RequestInterface $request): self
+    {
+        return match (true) {
+            $request instanceof ConnectRequest => self::Connect,
+            $request instanceof SubscribeRequest => self::Subscribe,
+            $request instanceof RefreshRequest => self::Refresh,
+            $request instanceof PublishRequest => self::Publish,
+            $request instanceof RPCRequest => self::RPC,
+            default => throw new InvalidRequestTypeException(
+                \sprintf('Request type `%s` is not supported', $request::class)
+            ),
+        };
+    }
 }
